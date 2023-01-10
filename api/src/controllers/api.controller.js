47e5721272b1d53e02fe47b2266a84544c0667b7
felduque <jf.duque557@gmail.com>
2023-01-10 15:07:12 -0800

    Api Finish with Axios

--- api/src/controllers/api.controller.js
+++ api/src/controllers/api.controller.js
@@ -1,4 +1,4 @@
-const { API_KEY } = process.env;
+// const { API_KEY } = process.env;
 import axios from "axios";
 
 const headers = {
@@ -34,20 +34,4 @@ async function getRecipes(req, res) {
 }
 
 export default getRecipes;
-// const { data } = await axios.get(
-//   `https://api.spoonacular.com/recipes/random?apiKey=44ff1d165b1d405181967465ef30ca64`,
-//   { headers }
-// );
 
-// export const writeData = data.recipes.map((r) => {
-//   return {
-//     id: r.id,
-//     name: r.title,
-//     image: r.image,
-//     summary: r.summary.replaceAll(/<(“[^”]”|'[^’]’|[^'”>])*>/g, ""),
-//     healthscore: r.healthScore,
-//     steps: r.analyzedInstructions[0].steps.map((s) => s.step),
-//     dishtypes: r.dishTypes,
-//     typeDiet: r.diets,
-//   };
-// });
