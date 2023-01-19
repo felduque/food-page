f6bdf3b2f1f42bbddf9acb516ef89e1764427643
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-01-18 18:21:21 -0800

    Update Backend

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,14 @@
 import app from "./app.js";
 import { sequelize } from "./database/db.js";
+import * as dotenv from 'dotenv'
+dotenv.config()
+const port = process.env.PORT || 3001;
 
 async function main() {
   try {
-    await sequelize.sync({ force: false });
-    app.listen(3002);
-    console.log("Server on port 3002");
+    await sequelize.sync({ force: true });
+    app.listen(port);
+    console.log(`Server on port ${port}`);
   } catch (error) {
     console.error("Error connecting to database", error);
   }
