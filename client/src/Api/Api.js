898787df105858feffbfc40725b5a5242ba5cc05
Felipe Duque <111407503+felduque@users.noreply.github.com>
2023-01-25 14:25:09 -0800

    Update api backend

--- client/src/Api/Api.js
+++ client/src/Api/Api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 export async function createRecipe(dataRecipe) {
   try {
-    await axios.post("http://localhost:3001/recipes", dataRecipe);
+    await axios.post("https://food-app.fly.dev/recipes/recipes", dataRecipe);
   } catch (err) {
     console.error(err, "Error en createRecipe");
   }
@@ -12,7 +12,7 @@ export async function createRecipe(dataRecipe) {
 
 export async function createAllInfo() {
   try {
-    await axios.post("http://localhost:3001/api/getallinfo");
+    await axios.post("https://food-app.fly.dev/recipes/api/getallinfo");
   } catch (err) {
     console.error(err, "Error en createAllInfo");
   }
@@ -20,7 +20,9 @@ export async function createAllInfo() {
 
 export async function getAllRecipes() {
   try {
-    const { data } = await axios.get("http://localhost:3001/recipes");
+    const { data } = await axios.get(
+      "https://food-app.fly.dev/recipes/recipes"
+    );
     return data;
   } catch (err) {
     console.error(err, "Error en getAllRecipes");
@@ -29,7 +31,9 @@ export async function getAllRecipes() {
 
 export async function getRecipeId(id) {
   try {
-    const { data } = await axios.get(`http://localhost:3001/recipes/id/${id}`);
+    const { data } = await axios.get(
+      `https://food-app.fly.dev/recipes/recipes/id/${id}`
+    );
     return data;
   } catch (err) {
     console.error(err, "Error en getRecipeId");
@@ -39,7 +43,7 @@ export async function getRecipeId(id) {
 export async function getRecipeByName(name) {
   try {
     const { data } = await axios.get(
-      `http://localhost:3001/recipes/name/${name}`
+      `https://food-app.fly.dev/recipes/recipes/name/${name}`
     );
     return data;
   } catch (err) {
@@ -51,7 +55,9 @@ export async function getRecipeByName(name) {
 
 export async function getAllTypeDiets() {
   try {
-    const { data } = await axios.get("http://localhost:3001/typediet");
+    const { data } = await axios.get(
+      "https://food-app.fly.dev/recipes/typediet"
+    );
     return data;
   } catch (err) {
     console.error(err, "Error en getAllTypeDiets");
@@ -60,7 +66,9 @@ export async function getAllTypeDiets() {
 
 export async function getTypeDietId(id) {
   try {
-    const { data } = await axios.get(`http://localhost:3001/typediet/${id}`);
+    const { data } = await axios.get(
+      `https://food-app.fly.dev/recipes/typediet/${id}`
+    );
     return data;
   } catch (err) {
     console.error(err, "Error en getTypeDietId");
